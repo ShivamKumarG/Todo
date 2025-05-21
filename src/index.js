5c0466c6fd04d724b2ee7b8703a50d9a8757e5a5
ShivamKumarG <91918756+ShivamKumarG@users.noreply.github.com>
2025-05-22 00:33:46 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import DisplayList from './components/DisplayList'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <TodoList /> */}
-    <DisplayList /> 
+    {/* <DisplayList />  */}
   </React.StrictMode>
 );
 
